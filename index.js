6cc76847619a9533520f909a5405a5f4a0c710c6
Sergio21678 <sergio.vilca@tecsup.edu.pe>
2025-05-13 22:26:49 -0500

    Proyecto completo con vistas y estilos modernos

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
